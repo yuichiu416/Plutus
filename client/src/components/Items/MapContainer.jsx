95d37215b30d7ff5e3c7caec6e9b3c8d8182bd0d
StanBond <stan.bond@ymail.com>
2019-10-09 15:56:02 -0600

    fixed show page bug

--- client/src/components/Items/MapContainer.jsx
+++ client/src/components/Items/MapContainer.jsx
@@ -4,30 +4,20 @@ import { Query } from "react-apollo";
 import queries from "../../graphql/queries";
 import { withRouter } from 'react-router-dom';
 import { geolocated } from "react-geolocated";
+import { translate } from 'react-switch-lang';
 
 const { FETCH_ITEMS } = queries;
 
 export class MapContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.stores = {};
-    }
-
-    displayMarkers = () => {
-        return Object.values(this.stores).map((store, index) => {
-            return <Marker key={index} id={index} position={{
-                        lat: store.lat,
-                        lng: store.lon
-                     }}
-                onClick={() => this.props.history.push(`/items/${store.id}`)}></Marker>
+    displayMarkers() {
+        return this.items.map((obj, index) => {
+            return <Marker key={index} id={index} position={JSON.parse(obj.location)}
+                onClick={() => this.props.history.push(`/items/${obj.id}`)}
+            ></Marker>
         })
     }
 
     render() {
-        const mapStyles = {
-            width: '100%',
-            height: '100%',
-        };
         return (
             <Query query={FETCH_ITEMS}>
                 {({ loading, error, data }) => {
@@ -35,16 +25,13 @@ export class MapContainer extends Component {
                     if (error) return `Error! ${error.message}`;
                     if (data.items.length === 0)
                         return <h1>No items yet</h1>
-                    data.items.forEach(item => {
-                        this.stores[item.name] = JSON.parse(item.location);
-                        this.stores[item.name].id = item.id;
-                    })
+                    this.items = data.items;
+                    const coords = this.props.coords || { latitude: 38.9551308, longitude: -92.34679059999999 }
                     return (
                         <Map
                             google={this.props.google}
                             zoom={12}
-                            style={mapStyles}
-                            initialCenter={{ lat: this.props.coords.latitude, lng: this.props.coords.longitude }}
+                            initialCenter={{ lat: coords.latitude, lng: coords.longitude }}
                         >
                             {this.displayMarkers()}
                         </Map>
@@ -54,7 +41,7 @@ export class MapContainer extends Component {
         );
     }
 }
-export default 
-    geolocated()(withRouter(GoogleApiWrapper({
+export default translate(geolocated()(
+    withRouter(GoogleApiWrapper({
         apiKey: 'AIzaSyDM9z1mv9d6Smdi3Z3vdDlUdabGYObzSvo'
-    })(MapContainer)));
+    })(MapContainer))));
