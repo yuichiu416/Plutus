c1f006625d9321760ee17912efe7a895840cdcf3
Roger Kiew <yuichiu416@gmail.com>
2019-10-11 12:57:46 -0500

    added some code for heroku deploy

--- server/server.js
+++ server/server.js
@@ -15,6 +15,14 @@ if (!db) {
     throw new Error("You must provide a string to connect to MongoDB Atlas");
 }
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
