608ac4c23dea2b63afb9e116f8488a6291f5be3b
Roger Kiew <yuichiu416@gmail.com>
2019-10-11 15:17:13 -0500

    update

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ if (!db) {
 }
 
 if (process.env.NODE_ENV === 'production') {
-    console.log("I'm in production");
     app.use(express.static('client/build'));
     app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
