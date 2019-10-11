4dac6662531f23cced467541258d49f568a8a6c7
Roger Kiew <yuichiu416@gmail.com>
2019-10-11 13:09:28 -0500

    delploy setup

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ if (!db) {
 }
 
 if (process.env.NODE_ENV === 'production') {
+    console.log("I'm in production");
     app.use(express.static('client/build'));
     app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
