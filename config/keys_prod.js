362a6f9506883a30000d442169a7aebbd2a8454c
Roger Kiew <yuichiu416@gmail.com>
2019-10-23 15:27:30 -0500

    deploy

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGO_URI: "mongodb+srv://yui:ruru780114@plutus-s09du.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
+    MONGO_URI: process.env.MONGO_URI,
+    secretOrKey: process.env.secretOrKey
 }
